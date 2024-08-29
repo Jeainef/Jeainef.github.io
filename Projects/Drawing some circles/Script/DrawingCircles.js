a85787a41eaae18786199b862b936e1b2731edc7
Jeainef <85844680+Jeainef@users.noreply.github.com>
2024-08-29 06:01:32 +0200

    Circles

--- Projects/Drawing some circles/Script/DrawingCircles.js
+++ Projects/Drawing some circles/Script/DrawingCircles.js
@@ -3,8 +3,6 @@
 
 var canvas = document.querySelector("canvas");
 
-canvas.width = window.innerWidth - 2;
-canvas.height = window.innerHeight - 2;
 
 
 var c = canvas.getContext("2d");
@@ -18,16 +16,21 @@ canvas.addEventListener("mousemove", function (event) {
     mouse.mouseY = event.y;
     console.log(mouse);
 });
+window.addEventListener("resize", function (event) {
+    init();
+})
 function Circle(x, y, radius, dx, dy, color) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
+    this.minRadius = radius;
     this.Draw = function () {
         c.beginPath()
         c.fillStyle = this.color;
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         c.fill()
+
     }
     this.Update = function () {
         this.x += dx;
@@ -46,23 +49,28 @@ function Circle(x, y, radius, dx, dy, color) {
 
 var circles = [];
 var colorArray = [
-    "Red", "Gray", "Black"
+    "#014040", "#02735E", "#03A678", "#F27405", "#731702"
 
 ]
-
-for (var i = 0; i < 1000; i++) {
-    radius = 5;
-    var x = Math.random() * (canvas.width - radius * 2) + radius;
-    var y = Math.random() * (canvas.height - radius * 2) + radius;
-    var dx = (Math.random() - 0.5) * 4
-    var dy = (Math.random() - 0.5) * 4
-    var color = colorArray[Math.floor(Math.random() * colorArray.length)]
-    circles.push(new Circle(x, y, radius, dx, dy, color))
+function init() {
+    circles = []
+    canvas.width = window.innerWidth - 2;
+    canvas.height = window.innerHeight - 2;
+    for (var i = 0; i < 1000; i++) {
+        radius = Math.random() * 5 + 1;
+        var x = Math.random() * (canvas.width - radius * 2) + radius;
+        var y = Math.random() * (canvas.height - radius * 2) + radius;
+        var dx = (Math.random() - 0.5) * 4
+        var dy = (Math.random() - 0.5) * 4
+        var color = colorArray[Math.floor(Math.random() * colorArray.length)]
+        circles.push(new Circle(x, y, radius, dx, dy, color))
+    }
 }
+
 var maxRadius = 25;
-var minRadius = 5;
 function animate() {
     requestAnimationFrame(animate);
+
     c.clearRect(0, 0, canvas.width, canvas.height);
     for (var i = 0; i < circles.length; i++) {
         circles[i].Update();
@@ -75,7 +83,7 @@ function animate() {
             }
         }
         else {
-            if (circles[i].radius > minRadius)
+            if (circles[i].radius > circles[i].minRadius)
                 circles[i].radius -= 1;
         }
 
@@ -83,6 +91,7 @@ function animate() {
 
 }
 
+init();
 animate();
 
 
