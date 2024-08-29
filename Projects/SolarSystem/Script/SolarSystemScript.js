18a32b6164b3f1caad48bd5d699603c21807ec27
Josep (Jeainef) <Jeainef@gmail.com>
2024-08-29 12:48:43 +0200

    Added the planet

--- Projects/SolarSystem/Script/SolarSystemScript.js
+++ Projects/SolarSystem/Script/SolarSystemScript.js
@@ -2,13 +2,42 @@ var canvas = document.querySelector("canvas");
 
 var c = canvas.getContext("2d");
 
-canvas.width = window.innerWidth - 2;
-canvas.height = window.innerHeight - 2;
+window.addEventListener("resize",function(){
+    Init();
+})
+canvas.addEventListener("mousemove",function(event){
+    mousePos.posX=event.x;
+    mousePos.posY=event.y;
+})
 
-//StyleVariables
-var starNumber = 300;
+//Variables
+
+var mousePos={
+    posX: window.innerWidth/2,
+    posY: window.innerHeight/2,
+}
+//Stars
+var stars = []
+var starColors = ["White", "Yellow", "Orange"]
+var starNumber = 100;
 var starMinRadius = 1;
 var starMaxRadius = 3;
+var starShimmerSpeed=0.03;
+
+//Planet
+var planet;
+var planetRadius=70;
+var mouseDrag=0.1;
+var planetColor="Red";
+
+//Sattelites
+var satellites=[]
+var satelliteColors = ["#FFAA00", "#FFBC36", "#CC8800","#002F75","#0052CC"]
+var SatelliteRadius=5;
+var satelliteSpeed=0.1;
+var maxSatelliteDistance=200;
+var minSatelliteDistance=130;
+var satelliteDrag=0.5;
 
 //Utilities
 
@@ -23,41 +52,136 @@ function Star(x, y, radius, color) {
     this.color = color;
     this.radiant = Math.random() * (2 * Math.PI);
 
+
+
+
     this.Draw = function () {
 
         c.beginPath();
+
+        c.fillStyle = this.color;
+        c.globalAlpha = Math.cos(this.radiant) * 0.5 + 0.5;
+        c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+        c.fill();
+    };
+    this.Update = function () {
+        this.radiant += starShimmerSpeed;
+
+        this.Draw();
+    };
+}
+function Planet(x, y, radius, color){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+
+    //Drag
+    this.lastPosition={x:this.x,y:this.y};
+
+    this.Draw = function () {
+
+        c.beginPath();
+        c.globalAlpha=1;
         c.fillStyle = this.color;
         c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
         c.fill();
     };
     this.Update = function () {
-        this.radiant += 0.005;
-        c.globalAlpha = Math.cos(this.radiant) * 0.5 + 0.5;
+
+        //DragEffect
+        this.lastPosition.x
+        +=(mousePos.posX - this.lastPosition.x)*mouseDrag
+        this.lastPosition.y
+        +=(mousePos.posY - this.lastPosition.y)*mouseDrag
+
+        this.x =  this.lastPosition.x;
+        this.y=  this.lastPosition.y;
+
         this.Draw();
     };
 }
+function Satellite(x, y, radius,color){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.radiant = Math.random() * (2 * Math.PI);
+    this.distance=RandomBetweenTwoValues(minSatelliteDistance,maxSatelliteDistance);
+    this.speed=RandomBetweenTwoValues(0.005,satelliteSpeed);
+    //Drag
+    this.lastPosition={x:x,y:y};
 
-var stars = []
-var starColors = ["White", "Yellow", "Orange"]
+    this.Draw = lastPoint =>{
 
+        c.beginPath();
+        c.globalAlpha=1;
+        c.strokeStyle = this.color;
+        c.lineWidth=this.radius;
+        c.moveTo(lastPoint.x,lastPoint.y);
+        c.lineTo(this.x,this.y);
+        c.stroke();
+        c.closePath();
+    };
+    this.Update = function ( TargetX, TargetY) {
 
-for (var int = 0; int < starNumber; int++) {
+        const lastPoint ={x:this.x,y:this.y}
+        this.radiant+=this.speed;
+        //DragEffect
+        this.lastPosition.x
+        +=(TargetX - this.lastPosition.x)*satelliteDrag
+        this.lastPosition.y
+        +=(TargetY - this.lastPosition.y)*satelliteDrag
 
-    var x = Math.random() * canvas.width;
-    var y = Math.random() * canvas.height;
-    var radius = RandomBetweenTwoValues(starMinRadius, starMaxRadius);
-    var color = starColors[Math.floor(Math.random() * starColors.length)];
-    console.log(color)
-    var star = new Star(x, y, radius, color);
-    stars.push(star);
+        this.x =  this.lastPosition.x + Math.cos(this.radiant) * this.distance;
+        this.y=  this.lastPosition.y + (Math.sin(this.radiant) * this.distance)/1.5;
+
+        this.Draw(lastPoint);
+    };
 }
 
-function animate() {
-    requestAnimationFrame(animate);
-    c.clearRect(0, 0, canvas.width, canvas.height);
-    for (var i = 0; i < stars.length; i++) {
-        stars[i].Update();
+function Init(){
+    canvas.width = window.innerWidth - 2;
+    canvas.height = window.innerHeight - 2;
+    stars=[];
+    planet= new Planet(canvas.width/2,canvas.height/2,planetRadius,planetColor)
+    for (var int = 0; int < starNumber; int++) {
+
+        var x = Math.random() * canvas.width;
+        var y = Math.random() * canvas.height;
+        var radius = RandomBetweenTwoValues(starMinRadius, starMaxRadius);
+        var color = starColors[Math.floor(Math.random() * starColors.length)];
+        var star = new Star(x, y, radius, color);
+        stars.push(star);
+    }
+
+
+    for (var int = 0; int < 300; int++) {
+
+        var x = window.innerWidth - 2 + Math.random()*10 + 5 + planetRadius;
+        var y = window.innerHeight - 2 + Math.random()*10 + 5+ planetRadius;
+        var radius = SatelliteRadius;
+        var color = satelliteColors[Math.floor(Math.random() * satelliteColors.length)];
+        var moon = new Satellite(x, y, radius, color);
+        satellites.push(moon);
     }
 }
 
-animate();
+
+function Animate() {
+    requestAnimationFrame(Animate);
+    c.fillStyle='rgba(0,0,0,0.07)'
+    c.fillRect(0, 0, canvas.width, canvas.height);
+  
+    for (var i = 0; i < stars.length; i++) {
+        stars[i].Update();
+    }
+    planet.Update();
+    for (var i = 0; i < satellites.length; i++) {
+        satellites[i].Update(planet.x,planet.y);
+    }
+
+}
+
+Init();
+Animate();
