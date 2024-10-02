797a7229245f5dd892f3f8cf3cdd150f097fdfcc
Jeainef <85844680+Jeainef@users.noreply.github.com>
2024-10-02 19:10:38 +0200

    Update Day_2.js

--- Projects/InkTober_2024/JS/Day_2.js
+++ Projects/InkTober_2024/JS/Day_2.js
@@ -1,25 +1,27 @@
-import { CreateShader,DrawRectangle,ClearViewport } from "./WebglHelperFunctions.js"
+import { CreateShader, DrawRectangle, ClearViewport } from "./WebglHelperFunctions.js"
 
-var canvas= document.getElementById("Canvas");
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight;
-var gl= canvas.getContext("webgl");
+var canvas = document.getElementById("Canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var gl = canvas.getContext("webgl");
 
 //Define shaders
 
 var vertexShader = /*glsl*/`
 
     uniform vec2 u_resolution;
-
+    uniform float u_time;
     attribute vec2 a_position;
 
     varying vec2 ul;
+    varying float time;
     void main(){
 
-        ul = (a_position/u_resolution);
-        ul = ul*2.0;
-        ul = ul - 1.0;
-        gl_Position = vec4(ul,0,1);
+        vec2 clipSpace= (a_position / u_resolution) * 2.0 - 1.0;
+        ul=clipSpace;
+        time=u_time;
+        ul.x = ul.x *  u_resolution.x/u_resolution.y;
+        gl_Position = vec4(clipSpace,0,1);
     }
 
 `;
@@ -27,11 +29,25 @@ var vertexShader = /*glsl*/`
 var fragmentShader =  /*glsl*/ `
     precision mediump float;
 
+    uniform vec2 u_resolution;
     varying vec2 ul;
+    varying float time;
 
     void main(){
 
-        gl_FragColor=vec4(ul,0,1);
+        vec2 fract_ul = ul;
+        vec3 finalColor=vec3(0.0);
+        
+        fract_ul= fract(fract_ul) - 0.5;
+        float distanceToCenter= length(fract_ul);
+        distanceToCenter= abs(sin(distanceToCenter *8.0 + time/1000.0)/8.0);
+
+        distanceToCenter = 0.1 / smoothstep(0.0,0.1,distanceToCenter);
+        distanceToCenter = fract(distanceToCenter);
+        float distanceToTrueCenter=abs(sin(length(ul) *8.0 - time/1000.0)/8.0);
+         distanceToTrueCenter= 0.1 / smoothstep(0.0,0.1,length(distanceToTrueCenter));
+        vec3 color =vec3(distanceToCenter,distanceToCenter * distanceToTrueCenter,distanceToCenter* distanceToTrueCenter);
+        gl_FragColor=vec4(color,1);
     }
 
 
@@ -40,16 +56,17 @@ var fragmentShader =  /*glsl*/ `
 
 //Start program
 
-var program= CreateShader(gl,fragmentShader,vertexShader);
+var program = CreateShader(gl, fragmentShader, vertexShader);
 gl.useProgram(program);
 
-ClearViewport(gl,canvas,[1,1,1,1]);
+ClearViewport(gl, canvas, [1, 1, 1, 1]);
 
 
 
 
-var positionAttributeLocation=gl.getAttribLocation(program,"a_position");
-var resolutionUniformLocation=gl.getUniformLocation(program,"u_resolution");
+var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+var timeLocation = gl.getUniformLocation(program, "u_time");
 
 var positionBuffer = gl.createBuffer();
 
@@ -59,6 +76,15 @@ gl.enableVertexAttribArray(resolutionUniformLocation);
 
 gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
-gl.vertexAttribPointer(positionAttributeLocation,2,gl.FLOAT, false,0,0)
+gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0)
 
-DrawRectangle(gl,0,0,canvas.width,canvas.height);
+
+function DrawLoop(timeStamp) {
+
+    gl.uniform1f(timeLocation, timeStamp);
+    DrawRectangle(gl, 0, 0, canvas.width, canvas.height);
+
+    window.requestAnimationFrame(DrawLoop);
+}
+
+window.requestAnimationFrame(DrawLoop);
