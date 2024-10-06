ccc6343935bbdaf16d9df0f4cdb2fecbca5df53f
Jeainef <85844680+Jeainef@users.noreply.github.com>
2024-10-06 18:12:20 +0200

    Day 6

---
+++ Projects/InkTober_2024/JS/Day_6.js
@@ -0,0 +1,105 @@
+import { CreateShader, ClearViewport, DrawRectangle } from "./WebglHelperFunctions.js";
+
+
+var canvas = document.getElementById("Canvas");
+
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+
+//Events
+
+window.addEventListener("mousemove", getMouseCoords);
+
+var mouseCoords = [
+    0, 0
+]
+
+function getMouseCoords(event) {
+    mouseCoords[0] = event.clientX;
+    mouseCoords[1] = event.clientY;
+    console.log(mouseCoords[1] / canvas.height * 2 - 1);
+}
+
+// Shader
+var gl = canvas.getContext("webgl");
+
+var vertexShader = /*glsl*/ `
+
+    attribute vec2 a_position;
+
+    uniform vec2 u_resolution;
+    uniform vec2 u_mouse;
+    uniform float u_time;
+
+
+    varying vec2 clipSpace;
+    varying vec2 mousePosition;
+    varying float time;
+    void main(){
+
+        mousePosition=(u_mouse/u_resolution) *2.0 - 1.0;
+        mousePosition.y=-mousePosition.y;
+        time=u_time/1000.0;
+        clipSpace = (a_position/u_resolution) * 2.0 - 1.0;
+        gl_Position = vec4(clipSpace,0,1);
+    }
+
+`
+
+var fragmentShader = /*glsl*/ `
+    precision mediump float;
+
+    varying vec2 clipSpace;
+    varying vec2 mousePosition;
+    varying float time;
+    void main(){
+        float mouselenght= distance(clipSpace,mousePosition);
+        mouselenght = smoothstep(0.01,0.7, mouselenght);
+        mouselenght += abs(sin(mouselenght*16. + time))/16.;
+        mouselenght = 0.05/ mouselenght;
+
+        
+        gl_FragColor=vec4(0,mouselenght,mouselenght,1);
+    }
+`
+
+var program = CreateShader(gl, fragmentShader, vertexShader);
+gl.useProgram(program);
+
+ClearViewport(gl, canvas, [1, 1, 1, 1]);
+
+var resolutionLocationID = gl.getUniformLocation(program, "u_resolution");
+gl.uniform2f(resolutionLocationID, canvas.width, canvas.height);
+
+
+//attributes
+
+var a_positionLocationID = gl.getAttribLocation(program, "a_position");
+var positionBuffer = gl.createBuffer();
+
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+gl.enableVertexAttribArray(positionBuffer);
+
+gl.vertexAttribPointer(a_positionLocationID, 2, gl.FLOAT, false, 0, 0);
+
+DrawRectangle(gl, 0, 0, canvas.width, canvas.height);
+
+var timeUniformID = gl.getUniformLocation(program, "u_time");
+var mousePosID = gl.getUniformLocation(program, "u_mouse");
+
+
+
+
+
+function DrawLoop(timeStamp) {
+
+    gl.uniform2f(mousePosID, mouseCoords[0], mouseCoords[1]);
+    gl.uniform1f(timeUniformID, timeStamp);
+
+    DrawRectangle(gl, 0, 0, canvas.width, canvas.height);
+
+    window.requestAnimationFrame(DrawLoop);
+}
+window.requestAnimationFrame(DrawLoop);
+
--- Projects/InkTober_2024/JS/WebglHelperFunctions.js
+++ Projects/InkTober_2024/JS/WebglHelperFunctions.js
@@ -1,32 +1,32 @@
 
 
 
-function CreateShader_source(gl,type,source){
+function CreateShader_source(gl, type, source) {
 
     var shader = gl.createShader(type); //Create a shader
 
-    gl.shaderSource(shader,source); //Specify the source (code of the shader)
+    gl.shaderSource(shader, source); //Specify the source (code of the shader)
     gl.compileShader(shader); //Compile the shader
 
     //test if it compiled correctly
-    var success= gl.getShaderParameter(shader,gl.COMPILE_STATUS);
-    if(success) return shader;
-    else console.log("There was an error creating the shader: " + source);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) return shader;
+    else console.log("There was an error creating the shader: " + gl.getShaderInfoLog(shader));
 }
 
-function CreateProgram(gl,vertex_shader,fragment_shader){
+function CreateProgram(gl, vertex_shader, fragment_shader) {
 
     var program = gl.createProgram(); //Create a new program
 
     //Attack the fragment and vertex shader
-    gl.attachShader(program,vertex_shader);
-    gl.attachShader(program,fragment_shader);
+    gl.attachShader(program, vertex_shader);
+    gl.attachShader(program, fragment_shader);
 
     gl.linkProgram(program); //Link the shaders to the program
 
     //Check if it works correctly
     var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-        if (success) return program;
+    if (success) return program;
     else console.log("The program failed to link. Check if the shaders are correct");
 }
 
@@ -39,42 +39,42 @@ function CreateProgram(gl,vertex_shader,fragment_shader){
  * vert = the vertex shader code
  * returns the program
  */
-export function CreateShader(context, frag, vert){
-    var fragmentShader = CreateShader_source(context,context.FRAGMENT_SHADER,frag);
-    var vertexShader = CreateShader_source(context,context.VERTEX_SHADER,vert);
+export function CreateShader(context, frag, vert) {
+    var fragmentShader = CreateShader_source(context, context.FRAGMENT_SHADER, frag);
+    var vertexShader = CreateShader_source(context, context.VERTEX_SHADER, vert);
 
-    var program=CreateProgram(context,vertexShader,fragmentShader);
+    var program = CreateProgram(context, vertexShader, fragmentShader);
     return program;
 }
 
-export function DrawRectangle(context,x,y,width,height){
-    var x1=x;
-    var x2=x+width;
+export function DrawRectangle(context, x, y, width, height) {
+    var x1 = x;
+    var x2 = x + width;
 
-    var y1=y;
-    var y2=y+height;
+    var y1 = y;
+    var y2 = y + height;
 
-    var positions=[
-        x1,y1,
-        x2,y1,
-        x2,y2,
-        x2,y2,
-        x1,y1,
-        x1,y2
+    var positions = [
+        x1, y1,
+        x2, y1,
+        x2, y2,
+        x2, y2,
+        x1, y1,
+        x1, y2
 
     ]
 
-    context.bufferData(context.ARRAY_BUFFER, new Float32Array(positions),context.STATIC_DRAW);
+    context.bufferData(context.ARRAY_BUFFER, new Float32Array(positions), context.STATIC_DRAW);
 
     context.drawArrays(context.TRIANGLES, 0, 6);
 
 }
 
-export function ClearViewport(context,canvas,color){
-    context.viewport(0,0,canvas.width,canvas.height);
+export function ClearViewport(context, canvas, color) {
+    context.viewport(0, 0, canvas.width, canvas.height);
     context.viewport(0, 0, context.canvas.width, context.canvas.height);
 
 
-    context.clearColor(color[0],color[1],color[2],color[3]);
+    context.clearColor(color[0], color[1], color[2], color[3]);
     context.clear(context.COLOR_BUFFER_BIT);
 }
