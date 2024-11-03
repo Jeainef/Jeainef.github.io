97dcc48f46eb63f985609fbf2707f2d430907c41
Jeainef <Jeainef@gmail.com>
2024-11-03 08:44:32 +0100

    updated voronoi

--- Projects/WebGL/JS/Shaders/VoronoiNoise.js
+++ Projects/WebGL/JS/Shaders/VoronoiNoise.js
@@ -5,6 +5,16 @@ const { vec2, vec3, mat3, mat4 } = glMatrix;
 //Get page values
 var canvas = document.getElementById("canvas");
 
+var timeSlider = document.getElementById("timeSlider");
+var sizeSlider = document.getElementById("sizeSlider");
+var repetitionSlider = document.getElementById("fractSlider");
+
+//Values
+var timeMultiplier = timeSlider.value/100;
+var tileSize = sizeSlider.value;
+var repeatingAmount = repetitionSlider.value;
+
+
 //Set Canvas size and context
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -18,8 +28,9 @@ var vertexShader= /*glsl*/ `
 
     varying vec2 st;
     void main(){
-        st = (a_position -0.5) * 2.;
-        gl_Position= vec4(st,0.,1.);
+        vec2 centeredCoords = (a_position -0.5) * 2.;
+        st=a_position;
+        gl_Position= vec4(centeredCoords,0.,1.);
     }
 `
 //Fragment Shader
@@ -32,10 +43,12 @@ var fragmentShader= /*glsl*/ `
     uniform vec2 u_resolution;
     uniform vec2 u_mousePos;
 
+    uniform float u_size;
+    uniform int u_repeating;
     varying vec2 st;
 
     vec2 rand2f(vec2 st){
-        float x=fract(sin(dot(st,vec2(233.32,155.32)))*9232.223);
+        float x=fract(sin(dot(st,vec2(233.32,155.32)))*(9232.223));
         float y=fract(sin(dot(st,vec2(541.32,212.32)))*9232.223);
         return vec2( x, y);
     }
@@ -44,26 +57,38 @@ var fragmentShader= /*glsl*/ `
         vec2 coords =st;
         coords.x *= u_resolution.x/u_resolution.y;
 
-        coords *=5.;
-        vec2 tileCoord = floor(coords);
-        vec2 pixelCoord= fract(coords);
+
 
         //Set initial colorS
         vec3 color= vec3(0.);
         
         float minDistance = 1.;
-        for(int x=-1;x<=1; x++){
-            for(int y=-1; y<=1; y++){
-                //We set the coords of the adjacent tile
-                vec2 adjacentTile = vec2(x,y);
-                vec2 adjacentPoint = rand2f(tileCoord + adjacentTile);
-                vec2 diff= adjacentTile + adjacentPoint - pixelCoord;
-                float dist = length(diff);
-
-                minDistance= min(minDistance,dist);
+        float finalminDistance = 1.;
+        int repetitions = u_repeating;
+        for(int z=1; z<=15; z++){
+            if(z>u_repeating) break;
+            vec2 tiledCoords =coords * u_size * float(z);
+            vec2 tileCoord = floor(tiledCoords);
+            vec2 pixelCoord= fract(tiledCoords);
+            for(int x=-1;x<=1; x++){
+                for(int y=-1; y<=1; y++){
+                    //We set the coords of the adjacent tile
+                    vec2 adjacentTile = vec2(x,y);
+                    vec2 adjacentPoint = rand2f(tileCoord  + adjacentTile);
+                    adjacentPoint =0.5 + 0.5 * sin(u_time + 645.343*adjacentPoint);
+                    vec2 diff= adjacentTile + adjacentPoint - pixelCoord;
+                    float dist = length(diff);
+    
+                    minDistance= min(minDistance,dist);
+                }
             }
+            finalminDistance=min(finalminDistance,minDistance);
+            color+=finalminDistance/(float(z));
         }
-        color+=minDistance;
+
+        vec3 backgroundColor = 1.- color;
+        //color= color* vec3(239./255.,228./255.,0.) + backgroundColor*vec3(0.,0.05,0.3);
+         //color += 1. - step(0.1,minDistance);
         gl_FragColor= vec4(color,1);
     }
 `
@@ -88,11 +113,31 @@ var timeUniformID = gl.getUniformLocation(program, "u_time");
 var resolutionUniformID = gl.getUniformLocation(program,"u_resolution");
 gl.uniform2f(resolutionUniformID,canvas.width,canvas.height);
 
+var repeatUniform = gl.getUniformLocation(program, "u_repeating");
+var tileSizeUniform = gl.getUniformLocation(program, "u_size");
 
+gl.uniform1f(tileSizeUniform,tileSize);
+gl.uniform1i(repeatUniform,repeatingAmount);
 function DrawLoop(timeStamp) {
-
-    gl.uniform1f(timeUniformID,timeStamp/1000);
+    gl.uniform1f(timeUniformID,timeStamp*timeMultiplier/1000);
+   
     DrawRectangle(gl,0,0,1,1);
     window.requestAnimationFrame(DrawLoop);
 }
 window.requestAnimationFrame(DrawLoop);
+
+timeSlider.oninput= function(){
+    timeMultiplier=this.value/100;
+    console.log(timeMultiplier);
+}
+sizeSlider.oninput= function(){
+    tileSize=this.value;
+    gl.uniform1f(tileSizeUniform,tileSize);
+    console.log(tileSize);
+}
+
+repetitionSlider.oninput= function(){
+    repeatingAmount=this.value;
+    gl.uniform1i(repeatUniform,repeatingAmount);
+    console.log(repeatingAmount);
+}
