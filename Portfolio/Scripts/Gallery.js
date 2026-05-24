85e672d2ac93232849853cbfccd167f7fa5b3d61
Jeainef <85844680+Jeainef@users.noreply.github.com>
2026-05-24 15:52:01 +0200

    Added images to gallery

--- Portfolio/Scripts/Gallery.js
+++ Portfolio/Scripts/Gallery.js
@@ -3,7 +3,7 @@
 var imageThumbs = document.getElementById("image-thumbs");
 var currentImage = document.getElementById("current-image");
 
-for (var i = 1; i <= 7; i++) {
+for (var i = 1; i <= 11; i++) {
     var thumb = document.createElement("img");
     thumb.src = "Images/Gallery/Image_" + i + ".png";
     thumb.alt = "Image " + i;
